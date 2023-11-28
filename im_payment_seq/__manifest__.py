edbfb1f0d615330e25cb60dea10d2ef98e64ba35
@maliaslam <odooclass21@gmail.com>
2023-11-28 20:27:10 +0500

    ir sequence update

--- im_payment_seq/__manifest__.py
+++ im_payment_seq/__manifest__.py
@@ -24,8 +24,8 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
         'data/sequence.xml',
+        'security/ir.model.access.csv',
         'views/view.xml',
     ],
     # only loaded in demonstration mode
