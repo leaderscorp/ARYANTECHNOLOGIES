7f75dedee58349d2e397e434e5c4e8e7ff0a8de6
@maliaslam <odooclass21@gmail.com>
2023-12-01 14:52:09 +0500

    add report customization

--- iman_pr_rep/models/models.py
+++ iman_pr_rep/models/models.py
@@ -7,9 +7,11 @@ class AccountPaymentsNumWords(models.Model):
     _inherit = 'account.payment'
 
     total_in_words = fields.Char(compute='int_to_words')
-
     @api.depends('amount_total')
     def int_to_words(self):
         for rec in self:
             rec.total_in_words = num2words(rec.amount, lang='en_US')
+
+
+
 
