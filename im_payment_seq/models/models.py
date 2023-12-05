b1fe5af55cde436bbab4b8a67859dd5d90efb290
@maliaslam <odooclass21@gmail.com>
2023-12-05 23:19:25 +0500

    add sequence based on currect date

--- im_payment_seq/models/models.py
+++ im_payment_seq/models/models.py
@@ -1,5 +1,5 @@
 from odoo import fields, api, models
-
+from datetime import datetime
 
 class IMAccountRegisterPayment(models.TransientModel):
     _inherit = 'account.payment.register'
@@ -49,6 +49,46 @@ class IMAccountPayment(models.Model):
         store=True
     )
 
+#     def action_post(self):
+#         res = super(IMAccountPayment, self).action_post()
+#
+#         # Payment sequence for customer
+#         for rec in self:
+#             if rec.custom_name is False and rec.move_id.custom_name is False:
+#                 if rec.is_internal_transfer is False:
+#                     if rec.journal_id.type == 'cash':
+#                         if rec.payment_type == 'outbound':  # Send
+#                             sequence_code = 'cash.payment.send.sequence'
+#                         elif rec.payment_type == 'inbound':  # Receive
+#                             sequence_code = 'cash.payment.receive.sequence'
+#                     elif rec.journal_id.type == 'bank':
+#                         if rec.payment_type == 'outbound':  # Send
+#                             sequence_code = 'bank.payment.send.sequence'
+#                         elif rec.payment_type == 'inbound':  # Receive
+#                             sequence_code = 'bank.payment.receive.sequence'
+#                 else:
+#                     sequence_code = 'internal.payment.sequence'
+#
+#                 # Generate sequence based on year and month from create_date
+#                 name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.date).next_by_code(sequence_code) or '/'
+#
+#                 if not rec.move_id.custom_name:
+#                     rec.move_id.custom_name = name
+#                 if not rec.custom_name:
+#                     rec.custom_name = name
+#
+#         return res
+#
+#     @api.returns('self', lambda value: value.id)
+#     def copy(self, default=None):
+#         if default is None:
+#             default = {}
+#
+#         if not default.get('custom_name'):
+#             default['custom_name'] = None
+#
+#         return super(IMAccountPayment, self).copy(default=default)
+
 
     def action_post(self):
         res = super(IMAccountPayment, self).action_post()
@@ -59,22 +99,22 @@ class IMAccountPayment(models.Model):
                     if rec.is_internal_transfer is False:
                         if rec.journal_id.type == 'cash':
                             if rec.payment_type == 'outbound':  # Send
-                                name = self.env['ir.sequence'].next_by_code('cash.payment.send.sequence') or '/'
+                                name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.date).next_by_code('cash.payment.send.sequence') or '/'
                                 # print('cash-send')
                             elif self.payment_type == 'inbound':  # Receive
-                                name = self.env['ir.sequence'].next_by_code('cash.payment.receive.sequence') or '/'
+                                name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.date).next_by_code('cash.payment.receive.sequence') or '/'
                                 # print('cash-receive')
 
                         elif rec.journal_id.type == 'bank':
                             if rec.payment_type == 'outbound':  # Send
-                                name = self.env['ir.sequence'].next_by_code('bank.payment.send.sequence') or '/'
+                                name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.date).next_by_code('bank.payment.send.sequence') or '/'
                                 # print('bank-send')
 
                             elif rec.payment_type == 'inbound':  # Receive
-                                name = self.env['ir.sequence'].next_by_code('bank.payment.receive.sequence') or '/'
+                                name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.date).next_by_code('bank.payment.receive.sequence') or '/'
                                 # print('bank-rec')
                     else:
-                        name = self.env['ir.sequence'].next_by_code('internal.payment.sequence') or '/'
+                        name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.date).next_by_code('internal.payment.sequence') or '/'
 
                     # rec.move_id.custom_name = name
                     # rec.custom_name = name
@@ -88,22 +128,22 @@ class IMAccountPayment(models.Model):
                     if rec.is_internal_transfer is False:
                         if rec.journal_id.type == 'cash':
                             if rec.payment_type == 'outbound':  # Send
-                                name = self.env['ir.sequence'].next_by_code('at.cash.payment.send.sequence') or '/'
+                                name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.date).next_by_code('at.cash.payment.send.sequence') or '/'
                                 # print('cash-send')
                             elif self.payment_type == 'inbound':  # Receive
-                                name = self.env['ir.sequence'].next_by_code('at.cash.payment.receive.sequence') or '/'
+                                name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.date).next_by_code('at.cash.payment.receive.sequence') or '/'
                                 # print('cash-receive')
 
                         elif rec.journal_id.type == 'bank':
                             if rec.payment_type == 'outbound':  # Send
-                                name = self.env['ir.sequence'].next_by_code('at.bank.payment.send.sequence') or '/'
+                                name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.date).next_by_code('at.bank.payment.send.sequence') or '/'
                                 # print('bank-send')
 
                             elif rec.payment_type == 'inbound':  # Receive
-                                name = self.env['ir.sequence'].next_by_code('at.bank.payment.receive.sequence') or '/'
+                                name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.date).next_by_code('at.bank.payment.receive.sequence') or '/'
                                 # print('bank-rec')
                     else:
-                        name = self.env['ir.sequence'].next_by_code('at.internal.payment.sequence') or '/'
+                        name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.date).next_by_code('at.internal.payment.sequence') or '/'
 
                     # rec.move_id.custom_name = name
                     # rec.custom_name = name
