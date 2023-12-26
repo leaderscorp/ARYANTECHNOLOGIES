6e2277fc480c2d346b82039cfad51d72bd870194
@maliaslam <odooclass21@gmail.com>
2023-12-26 15:19:32 +0500

    add module

---
+++ ol_accounting_balance_gl/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Ol Accounting Balance GL',
+    'version': '1.0.0',
+    'category': 'Cost',
+    'author':'Zain',
+    'sequence': -100,
+    'depends': ['account'],
+    'data': [
+        'views/journal_views.xml'
+        ],
+    'demo': [],
+    'application':True,
+    'installable': True,
+    'assets': {},
+    'license': 'LGPL-3',
+}
