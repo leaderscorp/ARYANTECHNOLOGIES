4c88078312fdeb20b8cca0bd8db5b8d75495bb92
@hafeezurk25 <hafeezurk25@gmail.com>
2024-06-12 14:14:16 +0500

    updated pivot test

--- im_inventory_rep_pivot_test/__manifest__.py
+++ im_inventory_rep_pivot_test/__manifest__.py
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'data/action.xml',
         'views/pivot.xml',
         'views/menu_item.xml',
         'views/stock_move.xml',
