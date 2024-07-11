d8a0468324ca7761ab02264863c04f00bd01c180
hammadularfeen <hammadarfeen1990@gmail.com>
2024-07-11 13:08:41 +0500

    add so num field in mo

--- im_inventory_rep_pivot_test/models/mrp_mo.py
+++ im_inventory_rep_pivot_test/models/mrp_mo.py
@@ -11,6 +11,10 @@ class im_mo_mrp(models.Model):
         compute='_onchange_user'
     )
 
+    so_num = fields.Char(
+        string='SO Num'
+    )
+
     def _onchange_user(self):
         for rec in self:
             rec.env_user = rec.env.user.id
