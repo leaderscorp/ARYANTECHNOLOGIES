2814855fa4afeca2a74f5f924a43bf5860f40c20
@hafeezurk25 <hafeezurk25@gmail.com>
2024-10-22 20:02:49 +0500

    added

--- iman_trans_hide_loc_from_user/models/models.py
+++ iman_trans_hide_loc_from_user/models/models.py
@@ -6,16 +6,38 @@ class iman_trans_hide_loc_from_user(models.Model):
     _inherit = 'stock.picking'
 
     loc_ids = fields.Many2many(
-        related="user_id.location_ids"
+        # related="user_id.location_ids"
+        compute='compute_loc_ids'
     )
+
+    def compute_loc_ids(self):
+        self.loc_ids = self.env.user.location_ids.ids
 
     dest_loc_ids = fields.Many2many(
-        related="user_id.dest_location_ids"
+        # related="user_id.dest_location_ids"
+        compute='compute_dest_loc_ids'
     )
 
+    def compute_dest_loc_ids(self):
+        self.dest_loc_ids = self.env.user.dest_location_ids.ids
+
+
     pick_ids = fields.Many2many(
-        related="user_id.picking_type_ids"
+        # related="user_id.picking_type_ids"
+        compute='compute_pick_ids'
     )
+    def compute_pick_ids(self):
+        self.pick_ids = self.env.user.picking_type_ids.ids
+
+    # def rec_to_user(self, check_rec_id, check_user_ids):
+    #         if check_rec_id is False:
+    #             pass
+    #         elif check_rec_id.id in check_user_ids:
+    #             pass
+    #         else:
+    #             print('else')
+    #             raise ValidationError(
+    #                 f"You are trying to use '{check_rec_id.display_name}' source location, but uou are only allowed to use these source location: \n {', '.join(check_user_ids.mapped('name'))}")
 
     @api.constrains('picking_type_id', 'user_id')
     def _check_valid_pick(self):
@@ -53,4 +75,3 @@ class iman_trans_hide_loc_from_user(models.Model):
                     pass
                 else:
                     raise ValidationError(f"You are trying to use '{rec.location_dest_id.display_name}' destination location, but you are only allowed to use these destination locations: \n  {', '.join(rec.dest_loc_ids.mapped('name'))}" )
-      
