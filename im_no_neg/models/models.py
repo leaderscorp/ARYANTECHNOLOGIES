d955430ddac9df018eb39de7e3ea61c8254dd0ac
Mujahid Hussain <mhq.icaew@gmail.com>
2026-04-29 11:57:33 +0500

    Update models.py

--- im_no_neg/models/models.py
+++ im_no_neg/models/models.py
@@ -34,6 +34,7 @@ class repair(models.Model):
                     #     raise UserError(message)
 
                 elif not prod_in_comp and comp.product_id.type == 'consu':
+                    
                     message = f"The location {comp.location_id.display_name} does not have the product {comp.product_id.name}"
                     raise UserError(message)
                 else:
