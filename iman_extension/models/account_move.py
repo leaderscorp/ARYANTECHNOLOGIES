9080428154b64be223b2f331bb2d50fb4cb21b0d
Mujahid Hussain <145117484+MujahidHussain1@users.noreply.github.com>
2026-05-20 22:31:57 +0500

    .

---
+++ iman_extension/models/account_move.py
@@ -0,0 +1,30 @@
+from odoo import models, fields, api
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    repair_number = fields.Char(
+        string="Repair Number",
+        compute="_compute_repair_number",
+    )
+
+    description_text = fields.Char(
+        string="Description",
+        index=True
+    )
+
+    site_location = fields.Char(
+        string="Site Location",
+    )
+
+    def _compute_repair_number(self):
+        for move in self:
+            sale = self.env['sale.order'].search([
+                ('name', '=', move.invoice_origin)
+            ], limit=1)
+
+            repair = self.env['repair.order'].search([
+                ('sale_order_id', '=', sale.id)
+            ], limit=1) if sale else False
+
+            move.repair_number = repair.seq_des if repair else False
